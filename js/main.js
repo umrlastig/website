3dfc2c9f939076d77fd1a58504250570b0a534db
gtouya <guillaume.touya@ign.fr>
2019-05-06 18:53:46 +0200

    template adaptation

--- js/main.js
+++ js/main.js
@@ -75,6 +75,7 @@ jQuery(function($) {
 			filter: ':not(.btn)'
 		});
 	});
+
 	// Window Scroll
 	function onScroll() {
 		if ($(window).scrollTop() > 50) {
@@ -152,7 +153,7 @@ jQuery(function($) {
 			bottomMargin = parseInt($dialog.css('marginBottom'), 10);
 
 		// Make sure you don't hide the top part of the modal w/ a negative margin
-		// if it's longer than the screen height, and keep the margin equal to 
+		// if it's longer than the screen height, and keep the margin equal to
 		// the bottom margin of the modal
 		if (offset < bottomMargin) offset = bottomMargin;
 		$dialog.css("margin-top", offset);
