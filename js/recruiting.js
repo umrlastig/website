59e0dafa0f5fda47d8f531b09d6864da166f1e13
Mathieu Brédif <mathieu.bredif@ign.fr>
2020-03-30 15:46:06 +0200

    Update recruiting.js

--- js/recruiting.js
+++ js/recruiting.js
@@ -21,121 +21,91 @@
 // THE SOFTWARE.
 
 // this file contains functions to handle the people.csv file of the members of LASTIG
-	function parseCSVfile() {
-		var url = "https://raw.githubusercontent.com/umrlastig/lastig_data/master/recruiting.csv";
-		var myInit = { method: 'GET'};
-		fetch(url,myInit)
-		.then(function(response) {
-			return response.ok ? response.text() : Promise.reject(response.status);
-		})
-		.then(function(text) {
-			var data = Papa.parse(text, {
-										download: false,
-										header: true,
-										worker: true,
-										step: function(row) {
-												//console.log("Row:", row.data);
-												if(row.data[0].type != ""){
-													if(row.data[0].type == "EC"){
-														var parent = document.getElementById("ec-offers");
-														divForPhD(parent, row.data);
-													}
-													else if(row.data[0].type == "PhD"){
-														var parent = document.getElementById("phd-offers");
-														divForPhD(parent, row.data);
-													}
-													else if(row.data[0].type == "postdoc"){
-														var parent = document.getElementById("postdoc-offers");
-														divForPhD(parent, row.data);
-													}
-													else{
-														if(row.data[0].team == "ACTE"){
-															var parent = document.getElementById("intern-acte");
-															divForIntern(parent, row.data);
-														}
-														else if(row.data[0].team == "MEIG"){
-															var parent = document.getElementById("intern-meig");
-															divForIntern(parent, row.data);
-														}
-														else if(row.data[0].team == "STRUDEL"){
-															var parent = document.getElementById("intern-strudel");
-															divForIntern(parent, row.data);
-														}
-														else if(row.data[0].team == "GEOVIS"){
-															var parent = document.getElementById("intern-geovis");
-															divForIntern(parent, row.data);
-														}
-													}
-												}
-										},
-										complete: function() {
-												console.log("display offers done!");
-										}
-								});
-			return data;
+function parseCSVfile() {
+	var url = "https://raw.githubusercontent.com/umrlastig/lastig_data/master/recruiting.csv";
+	var myInit = { method: 'GET'};
+	fetch(url,myInit)
+	.then(function(response) {
+		return response.ok ? response.text() : Promise.reject(response.status);
+	})
+	.then(function(text) {
+		var data = Papa.parse(text, {
+			download: false,
+			header: true,
+			worker: true,
+			step: function(row) {
+				//console.log("Row:", row.data);
+				if(row.data[0].type != ""){
+					if(row.data[0].type == "EC"){
+						var parent = document.getElementById("ec-offers");
+						divForPhD(parent, row.data);
+					}
+					else if(row.data[0].type == "PhD"){
+						var parent = document.getElementById("phd-offers");
+						divForJob(parent, row.data);
+					}
+					else if(row.data[0].type == "postdoc"){
+						var parent = document.getElementById("postdoc-offers");
+						divForJob(parent, row.data);
+					}
+					else{
+						if(row.data[0].team == "ACTE"){
+							var parent = document.getElementById("intern-acte");
+							divForJob(parent, row.data);
+						}
+						else if(row.data[0].team == "MEIG"){
+							var parent = document.getElementById("intern-meig");
+							divForJob(parent, row.data);
+						}
+						else if(row.data[0].team == "STRUDEL"){
+							var parent = document.getElementById("intern-strudel");
+							divForJob(parent, row.data);
+						}
+						else if(row.data[0].team == "GEOVIS"){
+							var parent = document.getElementById("intern-geovis");
+							divForJob(parent, row.data);
+						}
+					}
+				}
+			},
+			complete: function() {
+					console.log("display offers done!");
+			}
 		});
+		return data;
+	});
   };
 
-  function divForPhD(parentElement, data) {
-		// first remove the "no offer" line if still present
-		if(parentElement.firstElementChild.nodeName == "H5"){
-			parentElement.firstElementChild.remove();
-		}
+function divForJob(parentElement, data) {
+	// first remove the "no offer" line if still present
+	if(parentElement.firstElementChild.nodeName == "H5"){
+		parentElement.firstElementChild.remove();
+	}
 
-		const childElement = document.createElement('a');
-    const appendChildElement = parentElement.appendChild(childElement);
-    appendChildElement.setAttribute("class","list-group-item lang-fr");
-		appendChildElement.setAttribute("href", data[0].pdf);
-		appendChildElement.innerHTML = data[0].titre+"  ";
-		const childElementEn = document.createElement('a');
-		const appendChildElementEn = parentElement.appendChild(childElementEn);
-		appendChildElementEn.setAttribute("class","list-group-item lang-en");
-		appendChildElementEn.setAttribute("href", data[0].pdf);
-		appendChildElementEn.innerHTML = data[0].title+"  ";
+	const childElement = document.createElement('a');
+	const appendChildElement = parentElement.appendChild(childElement);
+	appendChildElement.setAttribute("class","list-group-item list-group-item-info lang-fr");
+	appendChildElement.setAttribute("href", data[0].pdf_fr);
+	appendChildElement.innerHTML = data[0].titre+"  ";
+	const childElementEn = document.createElement('a');
+	const appendChildElementEn = parentElement.appendChild(childElementEn);
+	appendChildElementEn.setAttribute("class","list-group-item list-group-item-info lang-en");
+	appendChildElementEn.setAttribute("href", data[0].pdf_en);
+	appendChildElementEn.innerHTML = data[0].title+"  ";
 
-		if(data[0].filled == "true"){
-			const filledElementEn = document.createElement('span');
-			filledElementEn.innerHTML = "filled offer";
-			filledElementEn.setAttribute("class","label label-success lang-en");
-			appendChildElementEn.appendChild(filledElementEn);
-			const filledElement = document.createElement('span');
-			filledElement.innerHTML = "offre pourvue";
-			filledElement.setAttribute("class","label label-success lang-fr");
-			appendChildElement.appendChild(filledElement);
-		}
-    //appendChildElement.innerHTML = data[0].status;
-  };
+	if(data[0].filled == "true"){
+		const filledElementEn = document.createElement('span');
+		filledElementEn.innerHTML = "filled offer";
+		filledElementEn.setAttribute("class","label label-success lang-en");
+		appendChildElementEn.appendChild(filledElementEn);
+		const filledElement = document.createElement('span');
+		filledElement.innerHTML = "offre pourvue";
+		filledElement.setAttribute("class","label label-success lang-fr");
+		appendChildElement.appendChild(filledElement);
+	}
+	//appendChildElement.innerHTML = data[0].status;
+};
 
-	function divForIntern(parentElement, data) {
-		// first remove the "no offer" line if still present
-		if(parentElement.firstElementChild.nodeName == "H5"){
-			parentElement.firstElementChild.remove();
-		}
-
-		const childElement = document.createElement('a');
-		const appendChildElement = parentElement.appendChild(childElement);
-		appendChildElement.setAttribute("class","list-group-item list-group-item-info lang-fr");
-		appendChildElement.setAttribute("href", data[0].pdf_fr);
-		appendChildElement.innerHTML = data[0].titre+"  ";
-		const childElementEn = document.createElement('a');
-		const appendChildElementEn = parentElement.appendChild(childElementEn);
-		appendChildElementEn.setAttribute("class","list-group-item list-group-item-info lang-en");
-		appendChildElementEn.setAttribute("href", data[0].pdf_en);
-		appendChildElementEn.innerHTML = data[0].title+"  ";
-
-		if(data[0].filled == "true"){
-			const filledElementEn = document.createElement('span');
-			filledElementEn.innerHTML = "filled offer";
-			filledElementEn.setAttribute("class","label label-success lang-en");
-			appendChildElementEn.appendChild(filledElementEn);
-			const filledElement = document.createElement('span');
-			filledElement.innerHTML = "offre pourvue";
-			filledElement.setAttribute("class","label label-success lang-fr");
-			appendChildElement.appendChild(filledElement);
-		}
-		//appendChildElement.innerHTML = data[0].status;
-	};
-
-	var displayRecruiting = function(){
+var displayRecruiting = function(){
     var data = parseCSVfile();
 };
